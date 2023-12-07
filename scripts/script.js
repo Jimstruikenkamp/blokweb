3fd60a28dc1cd06548f2f82f2224ec496797b0c5
Jimstruikenkamp <150832184+Jimstruikenkamp@users.noreply.github.com>
2023-12-07 15:55:34 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,10 @@
-// JavaScript Document
-console.log("hi");
+console.log("hiiiii");
+
+
+function openBurger() {
+  document.getElementById("hamburgerMenu").style.height = '100%';
+}
+
+function closeBurger() {
+  document.getElementById("hamburgerMenu").style.height = '0%';
+}
